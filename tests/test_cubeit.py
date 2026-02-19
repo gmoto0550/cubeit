0674ca28e04bc316cd640a324ba5e0d9ca9d5729
Tom Prasad <gmoto0550@gmail.com>
2026-02-19 12:12:44 +0530

    Parametrised

--- tests/test_cubeit.py
+++ tests/test_cubeit.py
@@ -1,15 +1,25 @@
 from playwright.sync_api import Page,expect
+import pytest
 BASE_URL='http://127.0.0.1:8000/cube.html'
-input_number = 10
-cube = input_number*input_number*input_number
-def test_cube(page:Page):
+
+@pytest.mark.parametrize(
+        ("number","cube"),
+        (
+            (10,1000),
+            (5,125),
+            (3,27),
+
+        )
+
+)
+def test_cube(page:Page,number,cube):
     page.goto(BASE_URL)
     input_field=page.get_by_placeholder("Enter a number")
-    input_field.fill(str(input_number))
+    input_field.fill(str(number))
     calculate_button=page.get_by_role("button",name="Calculate Cube")
     calculate_button.click()
     result=page.locator("div#result")
-    expect(result).to_contain_text(f"Cube of {input_number} is {cube}")
+    expect(result).to_contain_text(f"Cube of {number} is {cube}")
 def test_empty_input(page:Page):
     page.goto(BASE_URL)
     calculate_button=page.get_by_role("button",name="Calculate Cube")
